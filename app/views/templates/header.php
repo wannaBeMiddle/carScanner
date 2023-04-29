c1ed266bf5170639ab6e032d28ab9c2484c3be9c
Evgeniy Mukhin <muhin@intervolga.ru>
2023-04-29 16:28:36 +0300

    Подправил пути в шапке сайта

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -9,9 +9,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <link href="styles/#CSS#.css?time=<?=time()?>" rel="stylesheet">
+    <link href="/styles/#CSS#.css?time=<?=time()?>" rel="stylesheet">
 
-    <link rel="icon" href="assets/favicon.png" type="image/x-icon">
+    <link rel="icon" href="/assets/favicon.png" type="image/x-icon">
 
 </head>
 <body>
