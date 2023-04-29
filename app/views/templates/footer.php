4bf1b8a01232ac2a732f2a47729a9392968ddb3a
Evgeniy Mukhin <muhin@intervolga.ru>
2023-04-29 16:12:11 +0300

    Добавил header и footer для сайта

---
+++ app/views/templates/footer.php
@@ -0,0 +1,41 @@
+<?
+/**
+ * @var $user User
+ */
+
+use App\Modules\System\Container\Container;
+use App\Modules\System\User\User;
+
+$user = Container::getInstance()->get(User::class);
+?>
+<nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/"><img src="assets/logo-navbar.png" alt=""/></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="main.html">Данные</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Связь с менеджером</a>
+                </li>
+                <?if($user->isAuthorized()):?>
+                    <li class="nav-item dropup">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Профиль</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Личный кабинет</a></li>
+                            <li><a class="dropdown-item" href="#">Ввести показания вручную</a></li>
+                            <li><a class="dropdown-item text-danger" href="#">Выйти</a></li>
+                        </ul>
+                    </li>
+                <?endif;?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ app/views/templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>#TITLE#</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbar-bottom/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <link href="styles/#CSS#.css?time=<?=time()?>" rel="stylesheet">
+
+    <link rel="icon" href="assets/favicon.png" type="image/x-icon">
+
+</head>
+<body>
